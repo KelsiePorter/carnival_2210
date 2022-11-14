efd731c0abc4e800e8ca8dd63f5230d31340dfd1
Kelsie Porter <krporter22@gmail.com>
2022-11-14 10:01:05 -0700

    Create #board_rider, #reduce_spending_money, and #increase_total_revenue

--- lib/ride.rb
+++ lib/ride.rb
@@ -3,7 +3,8 @@ class Ride
               :min_height,
               :admission_fee,
               :excitement,
-              :total_revenue
+              :total_revenue,
+              :rider_log
   
   def initialize(ride_attributes)
     @name = ride_attributes[:name]
@@ -11,6 +12,20 @@ class Ride
     @admission_fee = ride_attributes[:admission_fee]
     @excitement = ride_attributes[:excitement]
     @total_revenue = 0
+    @rider_log = Hash.new(0)
   end
 
+  def board_rider(visitor)
+    @rider_log[visitor] += 1
+    reduce_spending_money(visitor)
+    increase_total_revenue
+  end
+
+  def reduce_spending_money(visitor)
+   visitor.spending_money -= @admission_fee
+  end
+
+  def increase_total_revenue
+    @total_revenue += @admission_fee
+  end
 end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -15,4 +15,41 @@ RSpec.describe Ride do
     expect(ride1.excitement).to eq(:gentle)
     expect(ride1.total_revenue).to eq(0)
   end
+
+  it 'can board a ride on the ride' do 
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+
+    expect(ride1.rider_log).to eq({visitor1 => 2, visitor2 => 1})
+  end
+
+  it 'reduces the visitors spending money every time they ride' do 
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+
+    expect(visitor1.spending_money).to eq(8)
+    expect(visitor2.spending_money).to eq(4)
+  end
+
+  it 'a rides revenue increases with each boarded rider' do 
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor1.add_preference(:gentle)
+    visitor2.add_preference(:gentle)
+    ride1.board_rider(visitor1)
+    ride1.board_rider(visitor2)
+    ride1.board_rider(visitor1)
+
+    expect(ride1.total_revenue).to eq(3)
+  end
 end
